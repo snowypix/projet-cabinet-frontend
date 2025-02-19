611aef71509444bbc4e1f2ef1cff5042a1351d7e
Yassine Zouin <opmail999@gmail.com>
2025-02-19 17:06:48 +0100

    5

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <footer className="bg-gray-200 text-gray-800 text-center py-4">
-            <div className="container mx-auto">
-                <p>Contact us: opmail999@gmail.com</p>
-                <p>© 2024 Yassine Center. All rights reserved.</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-gray-200 text-gray-800 text-center py-4">
+      <div className="container mx-auto">
+        <p>Contact us: opmail999@gmail.com</p>
+        <p>© 2024 Yassine Center. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
